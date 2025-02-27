2451f20e6bbad14d10f5f11916fc91350e1e41a2
Lasith Amanda <lasithamanda1999@example.com>
2025-02-27 14:48:56 +0530

    Add Context API

---
+++ task-manager/src/components/AuthContext.js
@@ -0,0 +1,35 @@
+import React, {createContext, useContext, useState, useEffect, Children} from 'react';
+import url from '../API/ApiService';
+
+const AuthContext = createContext();
+
+export const useAth =() =>{
+    return useContext(AuthContext);
+};
+
+export const AuthProvider = ({children}) => {
+    const [user,setUser] =useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token){
+            url.get('/auth/me').then((response)=> { 
+                setUser(response.data);
+            }).catch(()=>{
+                localStorage.removeItem('token');
+                setUser(null);
+            });
+        }
+    },[]);
+
+    const login = (userData, token) => {
+        localStorage.setItem('token',token);
+        setUser(userData);
+    };
+
+    return(
+        <AuthContext.Provider value={{user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
